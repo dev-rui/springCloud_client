fadcfb4524d174c7c723dcfd31460cdc5acc746a
heruihong0325 <18655377896@163.com>
2019-12-24 20:56:05 +0800

    新增服务跟踪

--- src/main/java/com/example/eurekaclient/controller/UserController.java
+++ src/main/java/com/example/eurekaclient/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.eurekaclient.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -8,13 +10,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Administrator
  */
 @RefreshScope
 @RestController
 public class UserController {
-
+   private static Logger logger= LoggerFactory.getLogger(UserController.class);
     @Autowired
     DiscoveryClient discoveryClient;
     @Value("${info.from}")
@@ -22,10 +26,11 @@ public class UserController {
 
 
     @GetMapping("/dc")
-    public String dc() throws InterruptedException {
+    public String dc(HttpServletRequest request) throws InterruptedException {
         //Thread.sleep(2000L);
         String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services);
+        logger.info("===请求client， TraceId={}, SpanId={}===",
+                request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
         return services;
     }
 
